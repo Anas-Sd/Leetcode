d301f23d33e6138072197099d0f7780ebda74f6d
Syed Anas <163738652+Anas-Sd@users.noreply.github.com>
2026-07-09 12:00:32 +0530

    Time: 2 ms (97.62%), Space: 44.4 MB (90.54%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfExist(int[] a) {
+        int n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j && a[i]==2*a[j])
+                return true;
+            }
+        }
+        return false;
+    }
+}
